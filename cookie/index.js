9cd6e0f48b75bc185154804e2e7d47eedd01e432
lonefy <lonefy@foxmail.com>
2018-05-23 15:18:24 +0800

    add cookie

---
+++ cookie/index.js
@@ -0,0 +1,20 @@
+/*
+ * cookie operation 
+ * author: Fuyu@lonefy
+ * create time: 2018-05-23
+ * 
+ */ 
+export default {
+    setCookie,
+    getCookie
+};
+
+function setCookie(name,value){
+    document.cookie = name + "="+ escape (value) + ";expires=Session" ;
+}        
+    
+function getCookie(str){
+    var tmp,reg=new RegExp("(^| )"+str+"=([^;]*)(;|$)","gi");
+    if(tmp=reg.exec(document.cookie))return(tmp[2]);
+    return null;
+}
