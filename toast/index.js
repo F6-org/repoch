ad4163fe865937343022686217756a979dc5bbcf
quanwei1 <quanwei1@staff.weibo.com>
2018-09-06 15:06:33 +0800

    update toast component

--- toast/index.js
+++ toast/index.js
@@ -1,120 +1,103 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import './styles.css';
-import Modal from '../modal';
+/*
+ * @param{
+ * show: bool
+ * text: string或dom皆可
+ * icon:'info/success/error/loading'
+ * autoHideDuration:INT 
+ * afterClose: ()=>{}
+ */
+import './style.scss';
+import React, {Component} from 'react';
+// import classNames from 'classnames';
 
-function noop() {}
-let timer = null;
-
-export default class Toast extends React.Component {
-    static propTypes = {
-        content: PropTypes.string.isRequired, // 组件提示文案
-        autohide: PropTypes.bool, // 是否自动隐藏
-        lasttime: PropTypes.number, // 组件展示时间
-        onShow: PropTypes.func, // 组件显示后回调函数
-        onHide: PropTypes.func, // 组件隐藏后的回调函数
-        style: PropTypes.object, // 属性自带样式继承
-        show: PropTypes.bool
+class Toast extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return this.props.show != nextProps.show || this.props.text != nextProps.text
     }
 
-
-    static defaultProps = {
-        autohide: true,
-        lasttime: 2000,
-        style: {},
-        show: false,
-        onHide: noop,
-        onShow: noop
+    componentWillUnmount(){
+        this.timer && clearTimeout(this.timer)
+        this.timer = null
     }
 
-    state = {
-        show: this.props.show
-    }
+    componentDidUpdate(){
+        let {autoHideDuration = 3000, afterClose, show} = this.props;
+        if(show){
+            this.timer = setTimeout(() => {
+                this.timer && clearTimeout(this.timer)
+                this.timer = null
 
-    componentWillReceiveProps = (nextProps) => {
-        if (this.props.show !== nextProps.show) {
-            return this.setState({ show: nextProps.show });
+                afterClose && afterClose();
+            }, autoHideDuration);
         }
-
-        if (this.props.content !== nextProps.content) {
-            this.setState({
-                show: nextProps.content === '' ? false : true
-            });
-        }
+   
     }
 
-    shouldComponentUpdate = (nextProps, nextStates) => {
-        if (this.state.show === nextStates.show) return false;
-
-        return true;
+    checkType() {
+        var params = [].slice.call(arguments);
+        var data = params[0];
+        var types = params.slice(1);
+        var _type = {}.toString.call(data).split(/\s|\]|\[/)[2].toLowerCase();
+        return types.length ? types.some(function (type) {
+            return _type.indexOf(type.toLowerCase());
+        }) > -1 : _type;
     }
 
-    componentDidMount = () => {
-        if (!this.props.autohide || !this.state.show) return;
-
-        let that = this;
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-            that.props.content && that.setState({ show: false });
-        }, this.props.lasttime);
+    handleTouch = e => {
+        
+        e.preventDefault();
+        e.stopPropagation();
+        e.nativeEvent.stopImmediatePropagation();
+        e.nativeEvent.preventDefault();
+        return false;
     }
 
-    componentDidUpdate = () => {
-        if (!this.props.autohide || !this.state.show) return;
-
-        let that = this;
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-            that.props.content && that.setState({ show: false });
-        }, this.props.lasttime);
-    }
-
-    render() {
-        let isShow = this.props.content ? (this.state.show ? true : false) : false;
-        let transitionClassOpts = {
-            styleName: 'fadelayer',
-            enteredClassName: 'fadein',
-            enteringClassName: 'fadein'
-        }
-
+    renderToast(){
+        const props = this.props;
+        const {className = "", icon = 'info', text, bgColor = 'rgba(51,51,51,.8)'} = props;
+       
         return (
-            <Modal
-                show={ isShow }
-                transition={ true }
-                isMiddle={ true }
-                onShow={ this.entered }
-                onHide={ this.exited }
-                transitionClassOpts={ transitionClassOpts }
-                onMaskClick={ this.handleMaskClick }
-                onEscapeKeyUp={ this.handleEscapeKeyUp }
-            >
-                <div className="layer_main_wrap" style={ this.props.style } key={ this.props.content }>
-                    <div className="layer_wrap">
-                    </div>
-                    <div className="inner">
-                        { this.props.content }
-                    </div>
-                </div>
-            </Modal>
+           
+            <div className={`c-toast-root ${className}`} style={this.getData('rootStyle')(bgColor)}>
+                <div className="icon">{this.getData('icon')(icon)}</div>
+                <div className="text">{text}</div>
+            </div>
+           
         );
     }
 
-    entered = () => {
-        this.props.onShow && this.props.onShow();
+    render() {
+        const {show, className = ""}= this.props;
+        return (
+                <div className={`c-toast-wrapper ${className} ${show? "in": "out"}`}
+                    onTouchMove={e => this.handleTouch(e)}
+                    onTouchEnd={e => this.handleTouch(e)}
+                >
+                    {
+                        this.renderToast()
+                    }
+                </div>
+        )
     }
 
-    exited = () => {
-        this.props.onHide && this.props.onHide();
-    }
-
-    handleMaskClick = () => {
-        clearTimeout(timer);
-        this.setState({ show: false });
-    }
-
-    handleEscapeKeyUp = () => {
-        clearTimeout(timer);
-        this.setState({ show: false });
+    getData(type) {
+        return {
+            'rootStyle': (bgColor) => ({
+                background: bgColor
+            }),
+            'icon': icon => {
+                if(!this.checkType(icon, 'string')){
+                    return icon;
+                }
+                return {
+                    'info': <i className="i-icon icon-info-circle"/>,
+                    'success': <i className="i-icon icon-check-circle"/>,
+                    'error': <i className="i-icon icon-error-circle"/>,
+                    'loading': <i className="i-icon icon-loading"/>,
+                }[icon]
+            }
+        }[type]
     }
 }
 
+export default Toast;
